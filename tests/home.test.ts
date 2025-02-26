f70a5bf48fbf838cdb8407e75a82dc8be73df92d
avinash-v2solutions <avinash.hediggond@v2solutions.com>
2025-02-26 13:22:09 +0530

    Add Files

---
+++ tests/home.test.ts
@@ -0,0 +1,51 @@
+import { test, expect} from "@playwright/test"
+import { title } from "process";
+test.describe('Home', () => { 
+
+    test (" Title of the Home Page", async({page})=>{
+        //Open the URL
+        await page.goto("https://practice.sdetunicorns.com/");
+
+        //Verify the Title
+        await expect(page).toHaveTitle("Practice E-Commerce Site – SDET Unicorns")
+
+    })
+
+    test("About Page", async({page})=>{
+
+        await page.goto("https://practice.sdetunicorns.com/about/")
+        await expect(page).toHaveTitle("About – Practice E-Commerce Site")
+
+        //print the title on console
+        console.log("Title is : " + await page.title())
+    })
+     
+    test("Use of CSS Selector and Verify URL", async({page})=>{
+        await page.goto("https://practice.sdetunicorns.com/")
+
+        //click on Get STarted
+        await page.locator('#get-started').click()
+
+        //Verify URL
+        await expect(page).
+        toHaveURL("https://practice.sdetunicorns.com/#get-started")
+
+        //print the URL
+        console.log("The URL is : "+await page.url())
+    
+    })
+
+
+    test("Use of Text Locator", async({page})=>{
+        await page.goto("https://practice.sdetunicorns.com/")
+
+        //find the Text (Element)
+        const headingText = await page.locator('text = Think different. Make different.')
+
+        //assert the text is visible
+        await expect(headingText).toBeVisible()
+
+        //print headingText
+        console.log("Heading is : "+ await headingText.textContent())
+    })
+})
